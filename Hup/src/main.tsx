008ecb304511bdc5adb667111c6828e5508dc51f
seifba007 <98665958+seifba007@users.noreply.github.com>
2025-03-25 22:32:41 +0100

    df

--- Hup/src/main.tsx
+++ Hup/src/main.tsx
@@ -5,8 +5,6 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from './core/store';
 import EntryPoint from './presentation/entryPoint/entryPoint';
 import '@mantine/core/styles.css';
-import '@mantine/charts/styles.css';
-import '@mantine/tiptap/styles.css';
 import './index.scss';
 import './core/i18n';
 import './sass/global.scss';
--- Hup/vite.config.ts
+++ Hup/vite.config.ts
@@ -1,24 +1,21 @@
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import ports from '../ports.config';
+import ports from '../ports.config'; 
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   css: {
     preprocessorOptions: {
       scss: {
-        // Ensure the path to `_mantine` is correct
-        additionalData: `@use "./src/styles/_mantine" as *;`,
+        additionalData: `@use "./src/_mantine" as *;`,
       },
     },
   },
   optimizeDeps: {
-    // Ensure this matches the actual dependency to exclude
     exclude: ['chunk-HU2LKPA7'],
   },
   server: {
-    // Add a fallback for the port if `ports.hupPort` isn't defined
-    port: ports?.hupPort || 3000,
+    port:  ports.hupPort , // Port controlled by the environment variable
   },
 });
